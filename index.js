589329df74eb0c09ffad1c3a01d6fe437a86a69e
uwebury <142432719+uwebury@users.noreply.github.com>
2023-11-25 13:25:45 +0100

    Separated App from index.js

--- index.js
+++ index.js
@@ -1,14 +1,4 @@
-import { Header } from "./components/Header/Header.js";
-import { Form } from "./components/Form/Form.js";
-import { CardList } from "./components/CardList/CardList.js";
-
-function App() {
-  const app = document.createElement("main");
-  app.classList.add("app");
-  app.append(Header(), Form(), CardList());
-
-  return app;
-}
+import { App } from "./components/App/App.js";
 
 // The render function renders the app to the DOM
 function render() {
